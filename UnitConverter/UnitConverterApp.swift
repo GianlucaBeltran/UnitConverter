62592cf9ff7e7582d3637018f4c143406ab87d07
GianlucaBeltran <gianlucabeltran@gmail.com>
2023-07-15 14:57:31 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Gianluca Beltran Bianchi on 15/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
